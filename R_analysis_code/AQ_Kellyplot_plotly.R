cd722a286685c0158edd001870a5db88fdee4949
lizadams <lizadams@email.unc.edu>
2025-06-12 10:47:21 -0400

    remove blank line to resolve error: unexpected end of input

--- R_analysis_code/AQ_Kellyplot_plotly.R
+++ R_analysis_code/AQ_Kellyplot_plotly.R
@@ -320,4 +320,4 @@ for (i in 1:6) {
 data.tmp <- data_melted.df[data_melted.df$variable == "NUM_OBS",]
 write.table(data.tmp,file=filename_txt,row.names=F,col.names=F,append=T,sep=",")
 ####################################
-
+}
